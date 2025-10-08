af378ed2a3ad616afa45b3ca8bcf552914890393
Sorta L⨀SŦ <aditya.funs.11@gmail.com>
2025-10-08 22:43:11 +0530

    fix config

--- src/config.py
+++ src/config.py
@@ -1,16 +1,16 @@
 import os
 
 DEBUG = os.getenv("DEBUG") or "False"
-USERNAME = os.getenv("username") or "admin"
-PASSWORD = os.getenv("password") or "default"
-SECRET_KEY = os.getenv("secret_key") or "secret123"
+USERNAME = os.getenv("USERNAME") or "admin"
+PASSWORD = os.getenv("PASSWORD") or "default"
+SECRET_KEY = os.getenv("SECRET_KEY") or "secret123"
 
-DATA_FILE = os.getenv("data_file") or "images.json" # where the data and image url will be stored on the other repo
-GITHUB_REPO = os.getenv("github_repo")
-GITHUB_REPO_BRANCH = os.getenv("github_repo_branch") or "master"
-GITHUB_EMAIL = os.getenv("github_email")
-GITHUB_USERNAME = os.getenv("github_username")
-GITHUB_TOKEN = os.getenv("github_token")
+DATA_FILE = os.getenv("DATA_FILE") or "images.json"  # where the data and image URL will be stored on the other repo
+GITHUB_REPO = os.getenv("GITHUB_REPO")
+GITHUB_REPO_BRANCH = os.getenv("GITHUB_REPO_BRANCH") or "master"
+GITHUB_EMAIL = os.getenv("GITHUB_EMAIL")
+GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GITHUB_API_URL = "https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}"
 
-DISPLAY_NAME = os.getenv("display_name") or USERNAME
+DISPLAY_NAME = os.getenv("DISPLAY_NAME") or USERNAME
